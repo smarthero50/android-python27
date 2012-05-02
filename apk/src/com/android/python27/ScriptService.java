c371f7564377208a8a0dc845f5830a3bc7900d16
anthony.prieur <anthony.prieur@gmail.com>
2012-05-02 15:10:44 +0200

    Fix env var for Python2.7

--- apk/src/com/android/python27/ScriptService.java
+++ apk/src/com/android/python27/ScriptService.java
@@ -156,10 +156,10 @@ public class ScriptService extends ForegroundService {
 		// env var
 		Map<String, String> environmentVariables = null;
 		environmentVariables = new HashMap<String, String>();
-	    environmentVariables.put("PYTHONPATH", Environment.getExternalStorageDirectory().getAbsolutePath()+ "/" + this.getPackageName() + "/extras/python" + ":" + this.getFilesDir().getAbsolutePath() + "/python/lib/python2.6/lib-dynload" + ":" + this.getFilesDir().getAbsolutePath() + "/python/lib/python2.6");
+	    environmentVariables.put("PYTHONPATH", Environment.getExternalStorageDirectory().getAbsolutePath()+ "/" + this.getPackageName() + "/extras/python" + ":" + this.getFilesDir().getAbsolutePath() + "/python/lib/python2.7/lib-dynload" + ":" + this.getFilesDir().getAbsolutePath() + "/python/lib/python2.7");
 	    environmentVariables.put("TEMP", Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + this.getPackageName() + "/extras/tmp");
 	    environmentVariables.put("PYTHONHOME", this.getFilesDir().getAbsolutePath() + "/python");
-	    environmentVariables.put("LD_LIBRARY_PATH", this.getFilesDir().getAbsolutePath() + "/python/lib" + ":" + this.getFilesDir().getAbsolutePath() + "/python/lib/python2.6/lib-dynload");			
+	    environmentVariables.put("LD_LIBRARY_PATH", this.getFilesDir().getAbsolutePath() + "/python/lib" + ":" + this.getFilesDir().getAbsolutePath() + "/python/lib/python2.7/lib-dynload");			
 		
 		File pythonBinary = new File(this.getFilesDir().getAbsolutePath() + "/python/bin/python");
 		
