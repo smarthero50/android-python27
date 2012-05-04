e11d398939ce7a103c0e2c0a4b5866c0f627bdd9
A Z <A.Z@Z.Z>
2012-05-04 16:53:25 +0200

    macos integration

--- python-build/bootstrap.sh
+++ python-build/bootstrap.sh
@@ -1,37 +1,52 @@
 #!/usr/bin/env sh
+set -e
+npath() { python -c "import sys, os; print os.path.normpath(os.path.abspath(os.path.join(*sys.argv[1:])))" $@; }
 
-ROOTDIR=$(dirname $(readlink -f $0))
+# ROOTDIR is the directory holding all the scripts configs, BUILDDIR is the current working dir (stage)
+export ROOTDIR=$( npath "$0" ".." )
+export BUILDDIR=$( pwd )
+. "$ROOTDIR/functions.sh" && readconfig "$ROOTDIR/config.sh" "$BUILDDIR/config.sh"
 
-if [ ! -e 'Python-2.7.2.tgz' ]; then
-    wget http://www.python.org/ftp/python/2.7.2/Python-2.7.2.tgz
+
+info "Bootstrapping python $PYVERSION for android with data from $ROOTDIR"
+
+if [ ! -e "$BUILDDIR/Python-2.7.2.tgz" ]; then
+    set_downloader
+    downloader http://www.python.org/ftp/python/2.7.2/Python-2.7.2.tgz
 fi
 
-if [ ! -e 'Python-host' ]; then
+if [ ! -e "$BUILDDIR/Python-host" ]; then
     tar zxvf Python-2.7.2.tgz
     mv Python-2.7.2 Python-host
 fi
 
-if [ ! -e 'Python' ]; then
+if [ ! -e "$BUILDDIR/Python" ]; then
     tar zxvf Python-2.7.2.tgz
     mv Python-2.7.2 Python
-    cd $ROOTDIR/Python
-    patch -p1 < ../patch/Python-2.7.2-xcompile.patch
-    patch -p1 < ../patch/Python-2.7.2-android.patch
-    patch -p1 -i ../patch/Python-2.7.2-site-relax-include-config.patch
+    cd "$BUILDDIR/Python"
+    
+    patch -p1 < "$ROOTDIR/patch/Python-2.7.2-xcompile.patch"
+    patch -p1 < "$ROOTDIR/patch/Python-2.7.2-android.patch"
+    patch -p1 -i "$ROOTDIR/patch/Python-2.7.2-site-relax-include-config.patch"
 fi
 
-if [ ! -e "$ROOTDIR/hostpython" -a ! -e "$ROOTDIR/hostpgen" ]; then
-    cd $ROOTDIR/Python-host
-    ./configure --prefix=$ROOTDIR/prebuilt
+
+if [ ! -e "$BUILDDIR/hostpython" -a ! -e "$BUILDDIR/hostpgen" ]; then
+    cd "$BUILDDIR/Python-host"
+    ./configure --prefix="$BUILDDIR/prebuilt"
     make -j4
     make install
-    mv python $ROOTDIR/hostpython
-    mv Parser/pgen $ROOTDIR/hostpgen
+    mv python "$BUILDDIR/hostpython"
+    mv Parser/pgen "$BUILDDIR/hostpgen"
     make distclean
 fi
 
-cd $ROOTDIR
-
-if [ ! -e 'openssl' ]; then
+if [ ! -e "$BUILDDIR/openssl" ]; then
+    cd "$BUILDDIR"
     git clone https://github.com/guardianproject/openssl-android.git openssl
+fi
+
+if [ ! -d "$BUILDDIR/toolchain" ]; then
+    [ ! -f "$NDK/build/tools/make-standalone-toolchain.sh" ] && error "missing \$NDK/build/tools/make-standalone-toolchain.sh"
+    "$NDK/build/tools/make-standalone-toolchain.sh" --platform=${ANDROID_PLATFORM} --install-dir="${BUILDDIR}/toolchain"
 fi
