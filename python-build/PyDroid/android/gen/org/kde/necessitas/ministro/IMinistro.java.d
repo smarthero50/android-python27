45c4457e71bb4f5a1a89130fa1a4effa887bff86
tarek sheasha <tarek.sheasha@gmail.com>
2012-07-30 00:48:59 +0200

    Removed auto gen files

--- python-build/PyDroid/android/gen/org/kde/necessitas/ministro/IMinistro.java.d
+++
@@ -1,4 +0,0 @@
-/home/tsheasha/GUC/Bachelors/PyDroid/PyDroidLauncher/PyDroid/android/gen/org/kde/necessitas/ministro/IMinistro.java: \
-  /home/tsheasha/GUC/Bachelors/PyDroid/PyDroidLauncher/PyDroid/android/src/org/kde/necessitas/ministro/IMinistro.aidl \
-  /home/tsheasha/GUC/Bachelors/PyDroid/PyDroidLauncher/PyDroid/android/src/org/kde/necessitas/ministro/IMinistroCallback.aidl 
-
--- python-build/PyDroid/android/gen/org/kde/necessitas/ministro/IMinistroCallback.java.d
+++
@@ -1,3 +0,0 @@
-/home/tsheasha/GUC/Bachelors/PyDroid/PyDroidLauncher/PyDroid/android/gen/org/kde/necessitas/ministro/IMinistroCallback.java: \
-  /home/tsheasha/GUC/Bachelors/PyDroid/PyDroidLauncher/PyDroid/android/src/org/kde/necessitas/ministro/IMinistroCallback.aidl 
-
