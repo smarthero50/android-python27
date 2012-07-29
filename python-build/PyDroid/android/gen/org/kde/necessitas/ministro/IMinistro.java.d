a44cfee8aa2e9a6ddf701b7b37b3512de8412407
tarek sheasha <tarek.sheasha@gmail.com>
2012-07-29 23:10:46 +0200

    PyQt4, Sip Folders already configured, a Necessitas based project called PyDroid that uses features from android-python27, build_py and config_py files that are required to build Python correctly since current ones did not work for me, finally a modified README with instructions on how to accomplish the PyQt4 port on Python 2.7.3 for Andorid

---
+++ python-build/PyDroid/android/gen/org/kde/necessitas/ministro/IMinistro.java.d
@@ -0,0 +1,4 @@
+/home/tsheasha/GUC/Bachelors/PyDroid/PyDroidLauncher/PyDroid/android/gen/org/kde/necessitas/ministro/IMinistro.java: \
+  /home/tsheasha/GUC/Bachelors/PyDroid/PyDroidLauncher/PyDroid/android/src/org/kde/necessitas/ministro/IMinistro.aidl \
+  /home/tsheasha/GUC/Bachelors/PyDroid/PyDroidLauncher/PyDroid/android/src/org/kde/necessitas/ministro/IMinistroCallback.aidl 
+
---
+++ python-build/PyDroid/android/gen/org/kde/necessitas/ministro/IMinistroCallback.java.d
@@ -0,0 +1,3 @@
+/home/tsheasha/GUC/Bachelors/PyDroid/PyDroidLauncher/PyDroid/android/gen/org/kde/necessitas/ministro/IMinistroCallback.java: \
+  /home/tsheasha/GUC/Bachelors/PyDroid/PyDroidLauncher/PyDroid/android/src/org/kde/necessitas/ministro/IMinistroCallback.aidl 
+
